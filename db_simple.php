9e9a6c4d9ddb43a102ebdd1f58d7230c6e0182ae
Rob Korobkin <rob.korobkin@gmail.com>
2018-03-23 10:38:43 -0400

    Configuring live environment.

--- db_simple.php
+++ db_simple.php
@@ -2,7 +2,30 @@
 
 	// connect to the database
 	global $db;
-	$db = new mysqli("localhost", "root", "root", "baxter_jobquery");
+
+	// if we're in production...
+	if(getcwd() == "/home/robkshar/jobquery.org") {
+
+		$server = "localhost";
+		$user 	= "robkshar_mtnbun";
+		$pw 	= "b@xt3r";
+		$db 	= "robkshar_jobquery";
+
+		$db = new mysqli($server, $user, $pw, $db);
+	}
+
+
+	// run this locally...
+	else {
+		$db = new mysqli("localhost", "root", "root", "baxter_jobquery");	
+	}
+
+
+
+	/////////////////////////////////////////////////////////////////////////////
+
+
+	// DEFINE BASIC DATA HANDLING OPERATIONS
 
 
 	function get_list($sql){
--- load_data.php
+++ load_data.php
@@ -1,17 +1,16 @@
 <?php
 
 	
-	// fetch jobs
+	// fetch employers
 	$sqle = 'SELECT * FROM employers';
 	$employers = get_list($sqle);
 
 
 
-
+	// fetch jobs
 	$sqlj = 	'SELECT * FROM jobs, employers
 				WHERE jobs.employerId = employers.employerId 
 				ORDER BY jobId DESC';
 
 	$jobs = get_list($sqlj);
 
-	print_r($jobs);
