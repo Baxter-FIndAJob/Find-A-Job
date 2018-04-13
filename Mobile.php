c5a8d69f426a552ee2983744ed4d422967c1eb6e
FMC03 <34164507+FMC03@users.noreply.github.com>
2018-04-13 10:45:05 -0400

    Add files via upload
    All work I have done on the mobile version (+ what Antonio did)

---
+++ Mobile.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Job Query</title>
+	<!-- <link href="client/css/mobile.css" rel="stylesheet" /> -->
+	<link href="client/css/mobile.css?v=<?php echo time();?>" type="text/css" rel="stylesheet">
+</head>
+<body>
+	<h1 class="title">Job Results</h1>
+	<div class="breadcrumb_menu">
+		<span class="text">All Jobs</span>
+		<img class="img" src="client/images/downarrow.png" />
+	</div>
+	<div>
+		<div class="job">
+		<h2 class="JOBtitles">McDonalds</body></h2>
+		<h4 class="Discription">I'm Lovin'it</h4>
+			<img src ="Plain-mcdonalds-logo.jpg" width="50" height="50"
+			alt="image not located">
+			<h5 class="JOBtype">Business</h5>
+        </div>
+        <div class="job">
+			<h2 class="JOBtitles">Mow My Lawn For $$$</body></h2>
+			<h4 class="Discription">I'm 44 and too fat to mow my lawn</h4>
+			<img src ="https://fthmb.tqn.com/dvG95UMBhOKsfPlreUToDu8P0ek=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-158412502-5816759c5f9b581c0b41ff9a.jpg" width="50" height="50"
+			alt="image not located">
+			<h5 class="JOBtype">Personal</h5>
+		</div>
+		<div class="job">
+			<h2 class="JOBtitles">Walmart</body></h2>
+			<h4 class="Discription">Save Money Live Better</h4>
+				<img src ="https://pbs.twimg.com/profile_images/872879465818243072/oHnYN1Jw_400x400.jpg" width="50" height="50"
+				alt="image not located">
+			<h5 class="JOBtype">Business</h5>
+		</div>
+		<div class="job">
+			<h2 class="JOBtitles">Subway</body></h2>
+			<h4 class="Discription">Eat Fresh</h4>
+				<img src ="http://thestamp.umd.edu/portals/0/Images/Food/subway.jpg" width="100" height="50"
+				alt="image not located">
+			<h5 class="JOBtype">Business</h5>
+		</div>
+		<div class="job">
+			<h2 class="JOBtitles">Paint my Porch</body></h2>
+			<h4 class="Discription">I can't paint like I used too</h4>
+				<img src ="https://www.theshabbycreekcottage.com/wp-content/uploads/2013/10/painted-porch.jpg" width="50" height="50"
+				alt="image not located">
+			<h5 class="JOBtype">Personal</h5>
+		</div>
+	</div>
+</body>
+</html>
