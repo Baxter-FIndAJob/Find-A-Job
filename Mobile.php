8dde35f5793e90fc76a98a8233f9a32551481961
Fred Czerwinski <frederick.czerwinski@baxter-academy.org>
2018-05-04 08:50:32 -0400

    mobile
    completed mobile files

--- Mobile.php
+++ Mobile.php
@@ -2,16 +2,18 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Job Query</title>
+	<title>Job Query-Home</title>
 	<!-- <link href="client/css/mobile.css" rel="stylesheet" /> -->
 	<link href="client/css/mobile.css?v=<?php echo time();?>" type="text/css" rel="stylesheet">
 </head>
 <body>
-	<h1 class="title">Job Results</h1>
-	<img class="glass" src="client/images/glasss.png">
-	<div class="breadcrumb_menu">
-		<span class="text">All Jobs</span>
-		<img class="img" src="client/images/downarrow.png" />
+	<div class="sticky_header">
+		<h1 class="title">Job Results</h1>
+		<img class="glass" src="client/images/glasss.png">
+		<div class="breadcrumb_menu">
+			<span class="text">All Jobs</span>
+			<img class="img" src="client/images/downarrow.png" />
+		</div>
 	</div>
 	<!-- FILL IN THE CODE BELOW TO ADD A JOB
 	<div class="job">
@@ -42,59 +44,158 @@
 			</div>
         </div>
 
-
         <div class="job">
-        	<div>
-        		<img class="JOBimg" src ="https://fthmb.tqn.com/dvG95UMBhOKsfPlreUToDu8P0ek=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-158412502-5816759c5f9b581c0b41ff9a.jpg" width="50" height="50" alt="image not located">
+			<div class="left">
+				<img class="JOBimg" src ="https://s3.amazonaws.com/elmenusV3/Photos/Normal/fbuhcacr.jpg" alt="image not located">
 			</div>
 
-			<div>
-				<h2 class="JOBtitles">Mow My Lawn For $$$</h2>
-				<h4 class="Discription">I'm 44 and too fat to mow my lawn</h4>
-				<div class="ns" class="s-space">Personal</div>
+			<div class="middle">
+				<div class="JOBtitles">McDonalds</div>
+				<div class="Discription">I'm Lovin'it</div>
+				<span class="s" class="s-space" >Business</span>
 			</div>
 
-			<div >
+			<div class="right">
 				<img class="arrow" src="client/images/arrow.png">
 			</div>
+        </div>
+        
 
 		<div class="job">
-			<div>
-				<img class="JOBimg" src ="https://pbs.twimg.com/profile_images/872879465818243072/oHnYN1Jw_400x400.jpg" width="50" height="50"
-				alt="image not located">
+			<div class="left">
+				<img class="JOBimg" src ="https://s3.amazonaws.com/elmenusV3/Photos/Normal/fbuhcacr.jpg" alt="image not located">
 			</div>
 
-			<div>
-				<h2 class="JOBtitles">Walmart</h2>
-				<h4 class="Discription">Save Money Live Better</h4>
-				<div class="s" class="s-space">Business</div>
+			<div class="middle">
+				<div class="JOBtitles">McDonalds</div>
+				<div class="Discription">I'm Lovin'it</div>
+				<span class="s" class="s-space" >Business</span>
 			</div>
 
-			<div>
+			<div class="right">
 				<img class="arrow" src="client/images/arrow.png">
 			</div>
+        </div>
+
+        <div class="job">
+			<div class="left">
+				<img class="JOBimg" src ="https://s3.amazonaws.com/elmenusV3/Photos/Normal/fbuhcacr.jpg" alt="image not located">
+			</div>
+
+			<div class="middle">
+				<div class="JOBtitles">McDonalds</div>
+				<div class="Discription">I'm Lovin'it</div>
+				<span class="s" class="s-space" >Business</span>
+			</div>
+
+			<div class="right">
+				<img class="arrow" src="client/images/arrow.png">
+			</div>
+        </div>
+
+        <div class="job">
+			<div class="left">
+				<img class="JOBimg" src ="https://s3.amazonaws.com/elmenusV3/Photos/Normal/fbuhcacr.jpg" alt="image not located">
+			</div>
+
+			<div class="middle">
+				<div class="JOBtitles">McDonalds</div>
+				<div class="Discription">I'm Lovin'it</div>
+				<span class="s" class="s-space" >Business</span>
+			</div>
+
+			<div class="right">
+				<img class="arrow" src="client/images/arrow.png">
+			</div>
+        </div>
+
+        <div class="job">
+			<div class="left">
+				<img class="JOBimg" src ="https://s3.amazonaws.com/elmenusV3/Photos/Normal/fbuhcacr.jpg" alt="image not located">
+			</div>
+
+			<div class="middle">
+				<div class="JOBtitles">McDonalds</div>
+				<div class="Discription">I'm Lovin'it</div>
+				<span class="s" class="s-space" >Business</span>
+			</div>
+
+			<div class="right">
+				<img class="arrow" src="client/images/arrow.png">
+			</div>
+        </div>
+
+        <div class="job">
+			<div class="left">
+				<img class="JOBimg" src ="https://s3.amazonaws.com/elmenusV3/Photos/Normal/fbuhcacr.jpg" alt="image not located">
+			</div>
+
+			<div class="middle">
+				<div class="JOBtitles">McDonalds</div>
+				<div class="Discription">I'm Lovin'it</div>
+				<span class="s" class="s-space" >Business</span>
+			</div>
+
+			<div class="right">
+				<img class="arrow" src="client/images/arrow.png">
+			</div>
+        </div>
+
+        <div class="job">
+			<div class="left">
+				<img class="JOBimg" src ="https://s3.amazonaws.com/elmenusV3/Photos/Normal/fbuhcacr.jpg" alt="image not located">
+			</div>
+
+			<div class="middle">
+				<div class="JOBtitles">McDonalds</div>
+				<div class="Discription">I'm Lovin'it</div>
+				<span class="s" class="s-space" >Business</span>
+			</div>
+
+			<div class="right">
+				<img class="arrow" src="client/images/arrow.png">
+			</div>
+        </div>
+
+        <div class="job">
+			<div class="left">
+				<img class="JOBimg" src ="https://s3.amazonaws.com/elmenusV3/Photos/Normal/fbuhcacr.jpg" alt="image not located">
+			</div>
+
+			<div class="middle">
+				<div class="JOBtitles">McDonalds</div>
+				<div class="Discription">I'm Lovin'it</div>
+				<span class="s" class="s-space" >Business</span>
+			</div>
+
+			<div class="right">
+				<img class="arrow" src="client/images/arrow.png">
+			</div>
+        </div>
+
+
+	</div>
+
+
+	<div class="footer">
+		<div class="footerNavItem">
+			<img class="footIMG" src="client/images/home.png" alt="image not located">
+			<div class="footTXT">HOME</div>
 		</div>
 
-		<div class="footer">
-			<div>
-				<img src="" alt="image not located">
-				<div>HOME</div>
-			</div>
+		<div class="footerNavItem">
+			<img class="footIMG" src="client/images/info.png" alt="image not located">
+			<div class="footTXT">INFO</div>
+		</div>
 
-			<div>
-				<img src="" alt="image not located">
-				<div>ABOUT</div>
-			</div>
+		<div class="footerNavItem">
+			<img class="footIMG" src="client/images/permit.png" alt="image not located">
+			<div class="footTXT">PERMIT</div>
+		</div>
 
-			<div>
-				<img src="" alt="image not located">
-				<div>PERMIT</div>
-			</div>
-
-			<div>
-				<img src="" alt="image not located">
-				<div>PROFILE</div>
-			</div>
+		<div class="footerNavItem">
+			<img class="footIMG" src="client/images/profile.png" alt="image not located">
+			<div class="footTXT">PROFILE</div>
 		</div>
 	</div>
 </body>
---
+++ mobilescreen2.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Job Query-Company</title>
+	<!-- <link href="client/css/mobile.css" rel="stylesheet" /> -->
+	<link href="client/css/mobile.css?v=<?php echo time();?>" type="text/css" rel="stylesheet">
+</head>
+<body>
+	<div class="sticky_header">
+		<h1 class="title">Company</h1>
+		<img class="glass" src="client/images/glasss.png">
+		<div class="breadcrumb_menu">
+			<span class="text">McDonalds</span>
+			<img class="img" src="client/images/downarrow.png" />
+		</div>
+	</div>
+
+		<div class="job">
+			<div class="left">
+				<img class="JOBimg" src ="https://s3.amazonaws.com/elmenusV3/Photos/Normal/fbuhcacr.jpg" alt="image not located">
+			</div>
+
+			<div class="middle">
+				<div class="JOBtitles">Frycook</div>
+				<div class="Discription">Makes the French Fries</div>
+				<span class="s" class="s-space" >Business</span>
+			</div>
+
+			<div class="right">
+				<img class="arrow" src="client/images/arrow.png">
+		</div>
+	</div>
+
+		<div class="job">
+			<div class="left">
+				<img class="JOBimg" src ="https://s3.amazonaws.com/elmenusV3/Photos/Normal/fbuhcacr.jpg" alt="image not located">
+			</div>
+
+			<div class="middle">
+				<div class="JOBtitles">Frycook</div>
+				<div class="Discription">Makes the French Fries</div>
+				<span class="s" class="s-space" >Business</span>
+			</div>
+
+			<div class="right">
+				<img class="arrow" src="client/images/arrow.png">
+		</div>
+	</div>
+
+		<div class="job">
+			<div class="left">
+				<img class="JOBimg" src ="https://s3.amazonaws.com/elmenusV3/Photos/Normal/fbuhcacr.jpg" alt="image not located">
+			</div>
+
+			<div class="middle">
+				<div class="JOBtitles">Frycook</div>
+				<div class="Discription">Makes the French Fries</div>
+				<span class="s" class="s-space" >Business</span>
+			</div>
+
+			<div class="right">
+				<img class="arrow" src="client/images/arrow.png">
+		</div>
+	</div>
+
+		<div class="job">
+			<div class="left">
+				<img class="JOBimg" src ="https://s3.amazonaws.com/elmenusV3/Photos/Normal/fbuhcacr.jpg" alt="image not located">
+			</div>
+
+			<div class="middle">
+				<div class="JOBtitles">Frycook</div>
+				<div class="Discription">Makes the French Fries</div>
+				<span class="s" class="s-space" >Business</span>
+			</div>
+
+			<div class="right">
+				<img class="arrow" src="client/images/arrow.png">
+		</div>
+	</div>
+
+		<div class="job">
+			<div class="left">
+				<img class="JOBimg" src ="https://s3.amazonaws.com/elmenusV3/Photos/Normal/fbuhcacr.jpg" alt="image not located">
+			</div>
+
+			<div class="middle">
+				<div class="JOBtitles">Frycook</div>
+				<div class="Discription">Makes the French Fries</div>
+				<span class="s" class="s-space" >Business</span>
+			</div>
+
+			<div class="right">
+				<img class="arrow" src="client/images/arrow.png">
+		</div>
+       </div>
+
+	<div class="footer">
+		<div class="footerNavItem">
+			<img class="footIMG" src="client/images/home.png" alt="image not located">
+			<div class="footTXT">HOME</div>
+		</div>
+
+		<div class="footerNavItem">
+			<img class="footIMG" src="client/images/info.png" alt="image not located">
+			<div class="footTXT">INFO</div>
+		</div>
+
+		<div class="footerNavItem">
+			<img class="footIMG" src="client/images/permit.png" alt="image not located">
+			<div class="footTXT">PERMIT</div>
+		</div>
+
+		<div class="footerNavItem">
+			<img class="footIMG" src="client/images/profile.png" alt="image not located">
+			<div class="footTXT">PROFILE</div>
+		</div>
+	</div>
+</body>
