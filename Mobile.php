9deb62ea2cf5ea5cd34e062d9aeeac672fb723f2
Mustafa Abu Hamad <mustafa.ah@baxter-academy.org>
2018-05-04 10:34:08 -0400

    new updates
    cool

--- Mobile.php
+++ Mobile.php
@@ -28,44 +28,12 @@
 
 
 		<!-- WORKING DRAFT -->
-		<div class="job">
-			<div class="left">
-				<img class="JOBimg" src ="https://s3.amazonaws.com/elmenusV3/Photos/Normal/fbuhcacr.jpg" alt="image not located">
-			</div>
-
-			<div class="middle">
-				<div class="JOBtitles">McDonalds</div>
-				<div class="Discription">I'm Lovin'it</div>
-				<span class="s" class="s-space" >Business</span>
-			</div>
-
-			<div class="right">
-				<img class="arrow" src="client/images/arrow.png">
-			</div>
-        </div>
-
-        <div class="job">
-			<div class="left">
-				<img class="JOBimg" src ="https://s3.amazonaws.com/elmenusV3/Photos/Normal/fbuhcacr.jpg" alt="image not located">
-			</div>
-
-			<div class="middle">
-				<div class="JOBtitles">McDonalds</div>
-				<div class="Discription">I'm Lovin'it</div>
-				<span class="s" class="s-space" >Business</span>
-			</div>
-
-			<div class="right">
-				<img class="arrow" src="client/images/arrow.png">
-			</div>
-        </div>
-        
 
 		<div class="job">
 			<div class="left">
 				<img class="JOBimg" src ="https://s3.amazonaws.com/elmenusV3/Photos/Normal/fbuhcacr.jpg" alt="image not located">
 			</div>
-
+			
 			<div class="middle">
 				<div class="JOBtitles">McDonalds</div>
 				<div class="Discription">I'm Lovin'it</div>
@@ -76,106 +44,6 @@
 				<img class="arrow" src="client/images/arrow.png">
 			</div>
         </div>
-
-        <div class="job">
-			<div class="left">
-				<img class="JOBimg" src ="https://s3.amazonaws.com/elmenusV3/Photos/Normal/fbuhcacr.jpg" alt="image not located">
-			</div>
-
-			<div class="middle">
-				<div class="JOBtitles">McDonalds</div>
-				<div class="Discription">I'm Lovin'it</div>
-				<span class="s" class="s-space" >Business</span>
-			</div>
-
-			<div class="right">
-				<img class="arrow" src="client/images/arrow.png">
-			</div>
-        </div>
-
-        <div class="job">
-			<div class="left">
-				<img class="JOBimg" src ="https://s3.amazonaws.com/elmenusV3/Photos/Normal/fbuhcacr.jpg" alt="image not located">
-			</div>
-
-			<div class="middle">
-				<div class="JOBtitles">McDonalds</div>
-				<div class="Discription">I'm Lovin'it</div>
-				<span class="s" class="s-space" >Business</span>
-			</div>
-
-			<div class="right">
-				<img class="arrow" src="client/images/arrow.png">
-			</div>
-        </div>
-
-        <div class="job">
-			<div class="left">
-				<img class="JOBimg" src ="https://s3.amazonaws.com/elmenusV3/Photos/Normal/fbuhcacr.jpg" alt="image not located">
-			</div>
-
-			<div class="middle">
-				<div class="JOBtitles">McDonalds</div>
-				<div class="Discription">I'm Lovin'it</div>
-				<span class="s" class="s-space" >Business</span>
-			</div>
-
-			<div class="right">
-				<img class="arrow" src="client/images/arrow.png">
-			</div>
-        </div>
-
-        <div class="job">
-			<div class="left">
-				<img class="JOBimg" src ="https://s3.amazonaws.com/elmenusV3/Photos/Normal/fbuhcacr.jpg" alt="image not located">
-			</div>
-
-			<div class="middle">
-				<div class="JOBtitles">McDonalds</div>
-				<div class="Discription">I'm Lovin'it</div>
-				<span class="s" class="s-space" >Business</span>
-			</div>
-
-			<div class="right">
-				<img class="arrow" src="client/images/arrow.png">
-			</div>
-        </div>
-
-        <div class="job">
-			<div class="left">
-				<img class="JOBimg" src ="https://s3.amazonaws.com/elmenusV3/Photos/Normal/fbuhcacr.jpg" alt="image not located">
-			</div>
-
-			<div class="middle">
-				<div class="JOBtitles">McDonalds</div>
-				<div class="Discription">I'm Lovin'it</div>
-				<span class="s" class="s-space" >Business</span>
-			</div>
-
-			<div class="right">
-				<img class="arrow" src="client/images/arrow.png">
-			</div>
-        </div>
-
-        <div class="job">
-			<div class="left">
-				<img class="JOBimg" src ="https://s3.amazonaws.com/elmenusV3/Photos/Normal/fbuhcacr.jpg" alt="image not located">
-			</div>
-
-			<div class="middle">
-				<div class="JOBtitles">McDonalds</div>
-				<div class="Discription">I'm Lovin'it</div>
-				<span class="s" class="s-space" >Business</span>
-			</div>
-
-			<div class="right">
-				<img class="arrow" src="client/images/arrow.png">
-			</div>
-        </div>
-
-
-	</div>
-
 
 	<div class="footer">
 		<div class="footerNavItem">
